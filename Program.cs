c28326825e076b1a22532f81cfa503183ace7522
Tommy Waalkes <Tommy@grandcircus.co>
2019-01-29 11:11:09 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangManStarterKit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Need to add in how to handle multi letter words like apple
+            HangmanGame hg = new HangmanGame();
+        }
+    }
+}
